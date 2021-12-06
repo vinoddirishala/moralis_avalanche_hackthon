5803ea739b34f5b904bf7df5339693bcd6e21084
Vinod Dirishala <22494324+vinoddirishala@users.noreply.github.com>
2021-12-06 18:07:27 +0530

    merge issues fix

--- src/App.js
+++ src/App.js
@@ -6,26 +6,10 @@ import NavBarComp from './components/navbar/NavBarComp';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Test1
-        </a>
-      </header>
-=======
+      
 
       <NavBarComp/>
 
->>>>>>> react bootstrap added
     </div>
   );
 }
