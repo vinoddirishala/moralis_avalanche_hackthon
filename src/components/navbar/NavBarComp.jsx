15c6e0479daeea04f715475ebe282af97ae9f267
Vinod Dirishala <22494324+vinoddirishala@users.noreply.github.com>
2021-12-06 18:45:09 +0530

    add navbar

--- src/components/navbar/NavBarComp.jsx
+++ src/components/navbar/NavBarComp.jsx
@@ -1,31 +1,55 @@
 import React,{Component} from 'react';
-import { Button,Container, Nav, Navbar } from 'react-bootstrap';
+import { Button,Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 export default class NavBarComp extends Component{
     render(){
         return(
             <>
-                    <Navbar bg="light">
+                    <Navbar bg="light" sticky="top">
                         <Container fluid>
 
                         <Navbar.Brand>
-                             Hackathon
+                             Moralis Avalanche Hackathon
                         </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="navbarScroll" />
-                            <Navbar.Collapse id="navbarScroll">
-                               
-                               <Nav>
-                               <Nav.Link href="#">Home</Nav.Link>
-                                <Nav.Link href="#">About</Nav.Link>
-                                <Nav.Link href="#">Contact</Nav.Link>
-                                <Nav.Link href="#">Faq</Nav.Link>
-                                
-                                <Nav.Item className="ml-auto">
-                                    <Button variant="warning">Connect Wallet</Button>
 
+                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+
+                            <Navbar.Collapse id="basic-navbar-nav">
+
+                               <Nav className="ms-auto">
+
+                                <Nav id="navBarMenuItemsList">
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="/about-us">About Us</Nav.Link>
+                                <Nav.Link href="/contact-us">Contact Us</Nav.Link>
+                                <Nav.Link href="/faq">Faq</Nav.Link>
+                                </Nav>
+
+                                <Nav.Item >
+                                    <Button variant="warning" id="nav-connect-wallet-btn">Connect Wallet</Button>
                                 </Nav.Item>
+
+                                <Nav.Item >
+                                    <NavDropdown title="0x8u********H89Y" id="nav-profile-dropdown">
+                                        <NavDropdown.Item>
+                                            Profile
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Divider/>
+                                        <NavDropdown.Item>
+                                            My Tickets
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Divider/>
+                                        <NavDropdown.Item>
+                                            Logout
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav.Item>
+
+
                                </Nav>
+
                                 
+                               
                                 
                             </Navbar.Collapse>
 
